7541bb356dda9b934e43153e246d937d7dc59c6c
RayMaroun <raymondmaroun15@gmail.com>
2024-05-10 20:32:32 +0300

    Update reinforcement week 5

---
+++ pluralsight/reinforcement/week-5/Week5Hotel/src/main/java/com/pluralsight/Hotel.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class Hotel {
+    private static int totalBookings = 0;
+
+    public static int getTotalBookings() {
+        return totalBookings;
+    }
+
+    public static void increaseTotalBookings() {
+        totalBookings++;
+    }
+
+    public static void decreaseTotalBookings() {
+        totalBookings--;
+    }
+}
